dea51f4c32f189e18a4cd375751b18ac97eacedb
H4RP3RK <kirsty.harper@zoho.com>
2020-05-02 08:08:49 +0000

    Fetch repos, github app

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,7 +2,7 @@ function userInformationHTML(user) {
     return `
     <h2>${user.name}
         <span class="small-name">
-            (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
         </span>
     </h2>
     <div class="gh-content">
@@ -13,6 +13,26 @@ function userInformationHTML(user) {
         </div>
         <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
     </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list"></div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    });
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Report List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
