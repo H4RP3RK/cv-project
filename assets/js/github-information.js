7049ac289c1caaf8ee8ff37bd5a9150a8af1a3bd
H4RP3RK <kirsty.harper@zoho.com>
2020-05-02 07:08:33 +0000

    Comparing my code to source code to fix bug

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -12,37 +12,40 @@ function userInformationHTML(user) {
             </a>
         </div>
         <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
-    </div>`
+    </div>`;
 }
 
 function fetchGitHubInformation(event) {
-    let username = $("#gh-username").val();
+    
+    var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
+
     $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading...">
-        </div>`
-    )
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function(firstResponse, secondResponse) {
-            let userData = firstResponse[0];
-            let repoData = secondResponse[0];
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("gh-user-data").html(userInformationHTML(userData));
             $("gh-repo-data").html(repoInformationHTML(repoData));
-        }, function(errorResponse) {
+        }, 
+        function(errorResponse) {
             if (errorResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
                 $("gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
-                );
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
